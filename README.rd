de000b98d73c9b1ca552bb3fe2b11790eec06aba
Jing Zhang <zhjxjtu@gmail.com>
2012-12-27 14:48:59 -0800

    First commit

---
+++ README.rd
@@ -0,0 +1,2 @@
+Contact:
+zhjxjtu@gmail.com
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Contactsup::Application.config.secret_token = 'f1f539b58ce762dc2b5e5a1f133e5d879a864dfe8da43e18e60f665b1a2b5f4f47da5b095657a0784074fd23727d01c4c99ebd564b8f43e4e59746cb70a76526'
