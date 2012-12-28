e1c234d0115f5cc9a00cd0c207e8f46c9ffc74bf
Jing Zhang <zhjxjtu@gmail.com>
2012-12-28 10:48:42 -0800

    Env #2: Set routes '/config/routes.rb' (Delete '/public/index.html')

--- config/routes.rb
+++ config/routes.rb
@@ -1,102 +1,29 @@
 Contactsup::Application.routes.draw do
-  get "contacts/index"
 
-  get "contacts/pending"
+  # Root
+  root to: 'webpages#landing'
 
-  get "contacts/create"
+  # Sessions controller
+  resources :sessions, only: [:new, :create, :destroy]
+  match '/signin',  to: 'sessions#new', via: :get
+  match '/signout', to: 'sessions#destroy', via: :delete
 
-  get "contacts/accept_signup_view"
+  # Users controller
+  resources :users, only: [:new, :create, :show, :update, :destroy]
+  match '/signup',  to: 'users#new', via: :get
+  match '/forgot_password',  to: 'users#forgot_password', via: :post
+  match '/reset_password',  to: 'users#reset_password_view', via: :get
+  match '/reset_password',  to: 'users#reset_password', via: :put
+  match '/update_password',  to: 'users#update_password', via: :put
 
-  get "contacts/accept_signup"
+  # Contacts controller
+  resources :contacts, only: [:index, :create]
+  match '/pending_contacts',  to: 'contacts#pending', via: :get
+  match '/accept_signup', to: 'contacts#accept_signup_view', via: :get
+  match '/accept_signup', to: 'contacts#accept_signup', via: :put
+  match '/accept_signin', to: 'contacts#accept_signin_view', via: :get
+  match '/accept_signin', to: 'contacts#accept_signin', via: :put
+  match '/block_contact', to: 'contacts#block', via: :post
+  match '/unblock_contact', to: 'contacts#unblock', via: :delete
 
-  get "contacts/accept_signin_view"
-
-  get "contacts/accept_signin"
-
-  get "contacts/block"
-
-  get "contacts/unblock"
-
-  get "users/new"
-
-  get "users/create"
-
-  get "users/show"
-
-  get "users/update"
-
-  get "users/destroy"
-
-  get "users/forgot_password"
-
-  get "users/update_password"
-
-  get "users/reset_password"
-
-  get "users/reset_password_view"
-
-  get "sessions/new"
-
-  get "sessions/create"
-
-  get "sessions/destroy"
-
-  get "webpages/landing"
-
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
 end
