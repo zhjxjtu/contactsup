47bc77830632e8fda6a6f3528391c2e94988ee30
Jing Zhang <zhjxjtu@gmail.com>
2012-12-28 10:20:06 -0800

    Test #0: Remove unit test folder (count=1)

--- spec/views/contacts/accept_signup_view.html.erb_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe "contacts/accept_signup_view.html.erb" do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
--- spec/views/users/forgot_password.html.erb_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe "users/forgot_password.html.erb" do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
